90eb5b7dace41fe2ebd5c8e74b614b6c339906e7
Devin Strachan <devstrach03@live.com>
2023-01-28 20:48:47 -0500

    Clean up content.js, remove extension directory

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,29 +1,22 @@
-// Modify if article elem present
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function wordArray(text) {
+    return text.replace(/[^A-Za-z]+/g, " ").trim().toLowerCase().split(" ");
+}
+
+function randomWordArray(text, numWords) {
+    let array = [];
+    let fullArray = wordArray(text);
+    for (let i = 0; i < numWords; i++) {
+        array.push(fullArray[Math.floor(Math.random() * fullArray.length)]);
+    }
+    return array;
+}
 
 const article = document.querySelector("article");
 if (article) {
-    const text = article.textContent;
-    const filtertext = text.replace(/[^A-Za-z]+/g, " ");
-    const wordArray = filtertext.trim().split(" ");
-    let randWordArray = [];
-    for (let i = 0; i < 10; i++) {
-       randWordArray.push(wordArray[Math.floor(Math.random() * (wordArray.length))]); 
-    }
+    let randWordArray = randomWordArray(article.textContent, 10);
     console.log(randWordArray);
-    const words = text.matchAll(/[^\s]+/g);
-    const sentences = text.matchAll(/[^\.]+/g);
-
-    const wordCount = [...words].length;
-    const sentenceCount = [...sentences].length;
-
-    const readingTime = Math.round(wordCount / 200);
-    const badge = document.createElement("p");
-    
-    badge.classList.add("color-secondary-text", "type--caption");
-    badge.textContent = `${readingTime} min read (word count ${wordCount}) (sentences ${sentenceCount}, random word)`;
-    
-    const heading = article.querySelector("h1");
-    const date = article.querySelector("time")?.parentNode;
-    
-    (date ?? heading).insertAdjacentElement("afterend", badge);
     }
