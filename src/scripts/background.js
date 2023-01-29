b572644aeaf48467352f3da1f441c7e9b413b11e
Devin Strachan <devstrach03@live.com>
2023-01-28 19:08:13 -0500

    Rework entire extension to grab ten random words

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,16 +1,18 @@
 // Extension state control flow
+const extensions = "https://developer.chrome.com/docs/extensions"
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
         text: "OFF",
     });
 });
-
-chrome.action.onClicked.addListener(async (tab) => {
-    const prevState = await chrome.action.getBadgeText({ tabID : tab.id});
+var currentState;
+chrome.action.onClicked.addListener(async () => {
+    const prevState = await chrome.action.getBadgeText({});
     const nextState = prevState === "ON" ? "OFF" : "ON";
+    currentState = prevState;
     
     await chrome.action.setBadgeText({
-        tabID : tab.id,
         text : nextState,
     })
-    });
+});
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,20 +1,29 @@
 // Modify if article elem present
+
 const article = document.querySelector("article");
-if (nextstate = "ON")
 if (article) {
     const text = article.textContent;
+    const filtertext = text.replace(/[^A-Za-z]+/g, " ");
+    const wordArray = filtertext.trim().split(" ");
+    let randWordArray = [];
+    for (let i = 0; i < 10; i++) {
+       randWordArray.push(wordArray[Math.floor(Math.random() * (wordArray.length))]); 
+    }
+    console.log(randWordArray);
     const words = text.matchAll(/[^\s]+/g);
+    const sentences = text.matchAll(/[^\.]+/g);
 
     const wordCount = [...words].length;
+    const sentenceCount = [...sentences].length;
+
     const readingTime = Math.round(wordCount / 200);
     const badge = document.createElement("p");
-
+    
     badge.classList.add("color-secondary-text", "type--caption");
-    badge.textContent = `${readingTime} min read (word count ${wordCount})`;
-
+    badge.textContent = `${readingTime} min read (word count ${wordCount}) (sentences ${sentenceCount}, random word)`;
+    
     const heading = article.querySelector("h1");
     const date = article.querySelector("time")?.parentNode;
-
+    
     (date ?? heading).insertAdjacentElement("afterend", badge);
-}
-console.log("test");
+    }
